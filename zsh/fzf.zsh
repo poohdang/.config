7284bcabf4314f428341f0bd95ae815998e42061
cuia <ndsczx@foxmail.com>
2020-06-20 13:20:54 +0800

    update for zsh

---
+++ zsh/fzf.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS='
+  --bind "ctrl-k:down,ctrl-i:up,ctrl-u:top,ctrl-y:execute-silent(echo {} | xclip -selection clipboard)"
+  --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (highlight -O xterm256 --style candy {} || cat {}) 2> /dev/null | head -100"
+'
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_COMPLETION_TRIGGER='\'
