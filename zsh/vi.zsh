87e90b733125acfdcade6d20689953d232124dab
cuia <ndsczx@foxmail.com>
2020-06-25 16:10:47 +0800

    update vi-mode for zsh

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -6,24 +6,17 @@ bindkey '^v' edit-command-line
 
 bindkey -v
 bindkey -M vicmd "h" vi-insert
+bindkey -M vicmd "H" vi-insert-bol
 bindkey -M vicmd "j" vi-backward-char
 bindkey -M vicmd "l" vi-forward-char
 bindkey -M vicmd "^j" vi-beginning-of-line
 bindkey -M vicmd "^l" vi-end-of-line
 bindkey -M vicmd "i" up-line-or-history
 bindkey -M vicmd "k" down-line-or-history
-#bindkey -M vicmd "K" vi-insert-bol
 bindkey -M vicmd "u" undo
 #bindkey -M vicmd "-" vi-rev-repeat-search
-# bindkey -M vicmd "=" vi-repeat-search
-#bindkey -M vicmd "g" vi-forward-word-end
-
-
-
-# use custom accept-line widget to update $VI_KEYMAP
-#bindkey -M vicmd '^J' vi-accept-line
-#bindkey -M vicmd '^M' vi-accept-line
-
+bindkey -M vicmd "=" vi-repeat-search
+bindkey -M vicmd "g" vi-forward-word-end
 
 # allow ctrl-p, ctrl-n for navigate history (standard behaviour)
 bindkey '^p' up-history
@@ -53,7 +46,7 @@ bindkey '^u' history-substring-search-up
 bindkey '^o' history-substring-search-down
 
 
-# allow ctrl-a and ctrl-e to move to beginning/end of line
+# allow ctrl-j and ctrl-l to move to beginning/end of line
 bindkey '^j' beginning-of-line
 bindkey '^l' end-of-line
 
@@ -91,7 +84,10 @@ _fix_cursor() {
 }
 precmd_functions+=(_fix_cursor)
 
+zle -N zle-line-init
+zle -N zle-keymap-select
 
+KEYTIMEOUT=1
 
 
 #function vi-accept-line() {
@@ -102,16 +98,3 @@ precmd_functions+=(_fix_cursor)
 #zle -N vi-accept-line
 
 
-# if mode indicator wasn't setup by theme, define default
-if [[ "$MODE_INDICATOR" == "" ]]; then
-  MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
-fi
-
-function vi_mode_prompt_info() {
-  echo "${${VI_KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
-}
-
-# define right prompt, if it wasn't defined by a theme
-if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
-  RPS1='$(vi_mode_prompt_info)'
-fi
